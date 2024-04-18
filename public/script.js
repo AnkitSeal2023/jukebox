b2e1652054a509474c9c2e9b2560449ad5c1b5bd
Ankit <ankitseal2021@gmail.com>
2024-04-18 18:18:04 +0530

    no modifications

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-console.log("script.js loaded");
+console.log(" loaded script.js");
 let rooms = [];
 let search, mainid, q, roomValue, requestOptions, contentInfo, timeline, time, tag, vidID, timerInterval;
 vidID="M7lc1UVf-VE";
